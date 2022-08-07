b0c9e50681059b83125071733dd2e3e788a2e6a7
Amory Galili <amorygalili@gmail.com>
2022-08-07 15:47:11 -0400

    Updated publish script. Removed caveat

--- create-plugin/src/cli.ts
+++ create-plugin/src/cli.ts
@@ -5,10 +5,10 @@ import { resolve } from 'path';
 
 const templateRoot = resolve(__dirname, '..', 'templates');
 
-const caveat = `
-This is a caveat!
-You can change this in \`src/cli.ts\`.
-`;
+// const caveat = `
+// This is a caveat!
+// You can change this in \`src/cli.ts\`.
+// `;
 
 // See https://github.com/uetchy/create-create-app/blob/master/README.md for other options.
 
@@ -25,5 +25,5 @@ create('create-plugin', {
   //   },
   // },
   // after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
-  caveat,
+  // caveat,
 });
