8c695499742b25965cd4392255b68c3079d2221f
Amory Galili <amorygalili@gmail.com>
2022-08-06 20:22:55 -0400

    Added svelte example

---
+++ svelte-example/src/App.svelte
@@ -0,0 +1,8 @@
+<svelte:options tag="my-app" />
+
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<my-counter></my-counter>
+
---
+++ svelte-example/src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<svelte:options tag="my-counter" />
+
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
