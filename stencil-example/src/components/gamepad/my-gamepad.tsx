86d8b1e1b9bc31f5db5a7b555d8b8b8fd37a5960
Amory Galili <amorygalili@gmail.com>
2022-08-21 05:17:34 -0400

    Added gamepad buttons and images

--- stencil-example/src/components/gamepad/my-gamepad.tsx
+++ stencil-example/src/components/gamepad/my-gamepad.tsx
@@ -1,4 +1,5 @@
 import { Component, h, getAssetPath } from '@stencil/core';
+import ButtonPad from './button-pad';
 
 @Component({
   tag: 'my-gamepad',
@@ -11,9 +12,14 @@ export class MyGamepad {
 
   render() {
     return (
-      <div>
-        <img src={getAssetPath('assets/gamepad.png')} />
-      </div>
+      <svg
+        viewBox="0 0 150 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <image href={getAssetPath('assets/gamepad.png')} width="150" height="100" />
+        <ButtonPad x={107} y={-20} leftPressed rightPressed />
+        <ButtonPad x={50} y={5} topPressed bottomPressed />
+      </svg>
     );
   }
 }
